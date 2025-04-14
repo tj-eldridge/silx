f9e7fa6724249e0ca88a1d52ccd83035527c5871
tj-eldridge <thomas.eldridge@esrf.fr>
2025-04-14 12:53:31 +0000

    change RELEV in _version.py

--- src/silx/_version.py
+++ src/silx/_version.py
@@ -70,7 +70,7 @@ PRERELEASE_NORMALIZED_NAME = {"dev": "a", "alpha": "a", "beta": "b", "candidate"
 MAJOR = 3
 MINOR = 0
 MICRO = 0
-RELEV = "dev"  # <16
+RELEV = "dev1"  # <16
 SERIAL = 0  # <16
 
 date = __date__
